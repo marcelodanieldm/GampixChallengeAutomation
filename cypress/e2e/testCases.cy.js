9391e50b5ffb32ae69ddda934c41132c41ef0e19
marcelodanieldm <danielmarcelo83@gmail.com>
2024-10-07 01:36:55 -0300

    TC1 TC2 TC3 listos

--- cypress/e2e/testCases.cy.js
+++ cypress/e2e/testCases.cy.js
@@ -18,10 +18,16 @@ const agregarAutoBtn = '.mb-3 > .btn';
 
 const buscarMarca = '#marca';
 const buscarBtn = 'body > div > main > div.card.mb-4.shadow-sm > div.card-body > form > div.row.mt-3 > div > button';
-const tablaRes = 'body > div > main > div.table-responsive';
-const editBtn = 'body > div > main > div.table-responsive > table > tbody > tr:nth-child(3) > td:nth-child(8) > a.btn.btn-warning.btn-sm';
+const tablaRes = 'body > div > main > div.table-responsive > table > tbody > tr > td:nth-child(1)';
+const editBtn = 'a.btn-warning'
 const guardarBtn = 'body > div > main > form > div:nth-child(10) > input';
 
+ const tituloHome = 'body > div > main > div.text-center.mb-4 > h1'
+ const tituloBusqueda = 'body > div > main > div.card.mb-4.shadow-sm > div.card-header.bg-light > h5'
+ const eliminarBtn = 'a.btn.btn-danger.btn-sm'
+ const confirmMensaje = 'h3';
+ const eliminarBtnConf = 'input[type="submit"].btn.btn-danger';
+ const alertBox = '.alert.alert-info';
 
 
 describe('Cinco test cases', () => {
@@ -32,30 +38,39 @@ describe('Cinco test cases', () => {
 
 //Ejecucion de tests
 
-  it.only('TC1 Agregar auto', () => {
+  it('TC1 Agregar auto', () => {
     cy.visit('https://frontend.wildar.dev/Autos')
     cy.get(agregarAutoBtn).contains('Registrar Nuevo Auto').click();
     cy.wait(2500);
     cy.get(marcaField).type('Toyota');
     cy.get(modeloField).type('Corolla');
-    cy.get(chasisField).type('ABC123456789');
+    cy.get(chasisField).type('123456789');
     cy.get(anioField).type('2022');
     cy.get(precioField).type('20.000,00');
     cy.get(colorField).type('Rojo');
     cy.get(estadoField).select('Nuevo');
     cy.get(crearBtn).click();
+    cy.wait(1000);
     //Buscar por marca
     cy.get(buscarMarca).type('Fiat');
     cy.get(buscarBtn).click();
     //asserts de resultado de busqueda.
-
-
   })
 
+  
   it('TC2 Modificar datos de un auto', () => {
     cy.visit('https://frontend.wildar.dev/Autos')
-    //agregando caso feliz
+    cy.wait(1000);
+    cy.clearLocalStorage();
+    cy.reload(true);
+    cy.getAllLocalStorage().should('be.empty');
+    //cy.clearCookies();
+    //cy.clearCookies().should('be.empty');
+    //cy.visit('https://frontend.wildar.dev/Autos')
     
+    
+    //agregando caso feliz
+    cy.wait(1500);
     cy.get(agregarAutoBtn).contains('Registrar Nuevo Auto').click();
     cy.wait(1500);
     cy.get(marcaField).type('Fiat');
@@ -66,34 +81,53 @@ describe('Cinco test cases', () => {
     cy.get(colorField).type('Amarillo');
     cy.get(estadoField).select('Nuevo');
     cy.get(crearBtn).click();
+    cy.wait(1000);
+
+    //validando el guardado de datos en el Home.
+    cy.get(tituloHome).contains('Registro de Automóviles');
+    cy.get(tituloBusqueda).should('be.visible');
+
     //buscar por marca
-    cy.get(tablaRes).contains('Fiat').
-    cy.get(editBtn).click();
-    cy.get(body > div > main > h1).should('have.text', 'Editar Auto');
-    get.wat(1500);
+
+    cy.get(buscarMarca).type('Fiat')
+    cy.get(tablaRes).contains('Fiat');
+    cy.get(editBtn).contains('Editar').click();
+    cy.wait(1500);
     //Modificar datos de precio, color y condicion
-    cy.get(precioField).type('500000');
-    cy.get(colorField).type('Rojo');
+    cy.get(precioField).clear().type('500000');
+    cy.get(colorField).clear().type('Rojo');
     cy.get(estadoField).select('Usado');
     cy.get(guardarBtn).click();
-    
-    //validacion de guardado exitoso
-
+    cy.wait(1000);
     
   })
 
-  it.skip('TC3 Eliminar auto de Inventario', () => {
+  it('TC3 Eliminar auto de Inventario', () => {
+    cy.visit('https://frontend.wildar.dev/Autos')
     //Buscar auto.
     cy.get(buscarMarca).type('Fiat');
     cy.get(buscarBtn).click();
     //validar que lo encontro
     //click en eliminar
+    //confirmacion de eliminar
+
+    //cy.get(confirmMensaje).should('have.text');
+    cy.get(eliminarBtn).click();
+    cy.wait(1000);
+    cy.get(eliminarBtnConf).should('have.value','Eliminar').click();
+    //cy.get('a[href="/Autos"]').should('have.text', 'Volver a la lista');
+    cy.wait(1500);
     //buscar auto eliminado
+    cy.get(buscarMarca).type('Fiat')
+    cy.get(buscarBtn).click();
     //validacion de auto eliminado
+    cy.get(alertBox).should('exist').contains('No se encontraron resultados para la búsqueda especificada.');
    
   })
+ 
 
-  it.skip('TC4 Buscar autos en Inventario', () => {
+  it.only('TC4 Buscar autos en Inventario', () => {
+    cy.visit('https://frontend.wildar.dev/Autos')
     cy.get(buscarMarca).type('Toyota');
     cy.get(buscarBtn).click();
     
